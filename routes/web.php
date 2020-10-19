e6614e0343225806fdfb643e9d5d78487737e5bc
Wu Yuzhe <a0903153387@yahoo.com.tw>
2020-10-20 01:14:23 +0800

    設定 Route 回傳字串

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect('r1');
+Route::get('/',function(){
+    return'welcome';
 });
-Route::get('r1', function () {
-    return redirect('r2');
-});
-Route::get('r2', function () {
-    return view('welcome');
-});
+
+
